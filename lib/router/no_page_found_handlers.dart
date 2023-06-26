7e8da1646fe115f80f1337360445fccf541e3f40
Jimmy Muñoz <jmsoftwaredv@hotmail.com>
2023-06-26 12:15:11 -0500

    Dashboard login and register

--- lib/router/no_page_found_handlers.dart
+++ lib/router/no_page_found_handlers.dart
@@ -3,7 +3,6 @@ import 'package:fluro/fluro.dart';
 import '../ui/views/no_page_found_view.dart';
 
 //Administrador de rutas de captura de errores 404 idependiente
-
 class NoPageFoundHandlers {
   //404
   static Handler noPageFound = Handler(handlerFunc: (context, parameters) {
